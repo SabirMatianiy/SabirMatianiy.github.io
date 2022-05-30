de30bf95b4534ab388c3c672bb8e1f489d5117a8
SabirMatianiy <matianiysamsung@gmail.com>
2022-05-30 22:44:17 +0600

    delete

--- project/js/arrow-scroll.js
+++
@@ -1,7 +0,0 @@
-let signy_section_footer_arrow_scroll = document.querySelector(".signy_section_footer_arrow_scroll");
-
-console.log(signy_section_footer_arrow_scroll);
-
-signy_section_footer_arrow_scroll.onclick = function() {
-	window.scrollTo(pageXOffset, 0);
-}
