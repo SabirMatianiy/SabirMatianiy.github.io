7f92c3c85ef9a0e87456a3b68e1aed50923722e7
SabirMatianiy <matianiysamsung@gmail.com>
2022-05-30 17:40:04 +0600

    delete

--- Минестерство образование КР/burger.js/burger.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$(".header_burger").click(function(event){
-		$(".header_burger , .header_menu").toggleClass("active");
-		});
-	$(".header_link").click(function(event){
-		$(".header_menu").remove("active");
-		$("header_burger , header_menu").toggleClass("active");
-	})
-});
